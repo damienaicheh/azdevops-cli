d56a39979377544ee1191c79cd02673d453898e4
gwendall garnier <gwendall.garnier@orange.com>
2022-11-23 18:06:59 +0100

    feat(unittest) : initial unittest integration

--- src/repos_updater/exceptions/repo_updater_exception.py
+++
@@ -1,6 +0,0 @@
-from exceptions.azdevops_exception import AzDevOpsException
-
-class RepoUpdaterException(AzDevOpsException):
-
-    def __init__(self, message):
-        super().__init__('Repo Updater', message)
