3766dc7440f520c7ef70174ed3d8b65279e32abe
Damien AICHEH <damien.aicheh@gmail.com>
2022-11-24 11:12:50 +0100

    feat: Add tests.

--- src/release_manager/tests/commands/cli/test_manifest_command.py
+++ src/release_manager/tests/commands/cli/test_manifest_command.py
@@ -1,10 +1,9 @@
-
 import os
 import unittest
 from unittest.mock import Mock
 
-from src.release_manager.commands.cli.manifest_command import ManifestCommand
-from src.release_manager.exceptions.release_manager_exception import ReleaseManagerException
+from release_manager.commands.cli.manifest_command import ManifestCommand
+from release_manager.exceptions.release_manager_exception import ReleaseManagerException
 
 class TestManifestCommand(unittest.TestCase):
 
@@ -12,49 +11,49 @@ class TestManifestCommand(unittest.TestCase):
         self.logger = Mock()
         self.command = ManifestCommand(self.logger)
 
-    def test_should_be_project_path_default(self):
+    def test_should_be_project_path_default(self) -> None:
        obj = {}
        actual = os.getcwd()
        excepted = self.command.get_project_path(obj)
        self.assertEqual(actual, excepted)
 
-    def test_should_be_project_path_from_cli(self):
+    def test_should_be_project_path_from_cli(self) -> None:
        obj = {'project_path': os.path.join(os.getcwd(),'../') }
        actual = os.path.join(os.getcwd(),'../')
        excepted = self.command.get_project_path(obj)
        self.assertEqual(actual, excepted)
 
-    def test_should_be_project_path_throw_exception_not_valid(self):
+    def test_should_be_project_path_throw_exception_not_valid(self) -> None:
         with self.assertRaises(ReleaseManagerException) as ex:
             obj = {'project_path': os.path.join(os.getcwd(),'../a') }
-            self.command.get_output(obj)
+            self.command.get_project_path(obj)
         self.assertTrue('not valid' in ex.exception.message)
 
-    def test_should_be_application_name_from_parameter(self):
+    def test_should_be_application_name_from_parameter(self) -> None:
        obj = {'application_name': 'azdevops-cli'}
        actual = 'azdevops-cli'
        excepted = self.command.get_application_name(obj)
        self.assertEqual(actual, excepted)
 
-    def test_should_be_application_name_throw_exception_required(self):
+    def test_should_be_application_name_throw_exception_required(self) -> None:
         with self.assertRaises(ReleaseManagerException) as ex:
             obj = {}
-            self.command.get_output(obj)
+            self.command.get_application_name(obj)
         self.assertTrue('required' in ex.exception.message)
 
-    def test_should_be_output_default(self):
+    def test_should_be_output_default(self) -> None:
        obj = {}
        actual = os.getcwd()
        excepted = self.command.get_output(obj)
        self.assertEqual(actual, excepted)
 
-    def test_should_be_output_from_cli(self):
+    def test_should_be_output_from_cli(self) -> None:
        obj = {'output': os.path.join(os.getcwd(),'../') }
        actual = os.path.join(os.getcwd(),'../')
        excepted = self.command.get_output(obj)
        self.assertEqual(actual, excepted)
 
-    def test_should_be_output_throw_exception_not_valid(self):
+    def test_should_be_output_throw_exception_not_valid(self) -> None:
         with self.assertRaises(ReleaseManagerException) as ex:
             obj = {'output': os.path.join(os.getcwd(),'../a') }
             self.command.get_output(obj)
--- src/repo_updater/exceptions/repo_updater_exception.py
+++ src/repo_updater/exceptions/repo_updater_exception.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from exceptions.azdevops_exception import AzDevOpsException
 
 class RepoUpdaterException(AzDevOpsException):
