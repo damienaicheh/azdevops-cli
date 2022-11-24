3d0aa61429cf673f69ad77def48b5bb29ae6d5d4
gwendall garnier <gwendall.garnier@orange.com>
2022-11-24 18:33:52 +0100

    feat(unittest) : initial unittest integration

--- src/release_manager/helpers/manifest_generator.py
+++ src/release_manager/helpers/manifest_generator.py
@@ -4,7 +4,6 @@
 import json
 import git
 import os
-from helpers.envs import get_env
 
 def generate_manifest(project_path: str, application_name: str, output: str):
     git_client = git.Git(project_path if project_path != None else os.path)
@@ -16,11 +15,11 @@ def generate_manifest(project_path: str, application_name: str, output: str):
 
     manifest = {
         "ApplicationName": application_name,
-        "PipelineName" : get_env("BUILD_DEFINITIONNAME"),
-        "BuildId": get_env("BUILD_BUILDID"),
-        "BuildNumber": get_env("BUILD_BUILDNUMBER"),
-        "SourceBranchName": get_env("BUILD_SOURCEBRANCHNAME"),
-        "Scm": get_env("BUILD_REPOSITORY_NAME"),
+        "PipelineName" : os.getenv("BUILD_DEFINITIONNAME"),
+        "BuildId": os.getenv("BUILD_BUILDID"),
+        "BuildNumber": os.getenv("BUILD_BUILDNUMBER"),
+        "SourceBranchName": os.getenv("BUILD_SOURCEBRANCHNAME"),
+        "Scm": os.getenv("BUILD_REPOSITORY_NAME"),
         "Sha1": initial_commit,
         "LatestTag": lastest_tag
     }
