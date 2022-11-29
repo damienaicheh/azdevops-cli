44ccd137eec9621eeaf8e13c26dfd003819cb64c
Damien AICHEH <damien.aicheh@gmail.com>
2022-11-29 11:34:58 +0100

    feat: Add dynamic version.

--- src/models/version_infos.py
+++ src/models/version_infos.py
@@ -1,4 +1,9 @@
+import importlib.metadata
+
 class VersionInfos(object):
     def __init__(self):
-        self.version = '1.0.0'
+        self.version = '0.0.0'
         self.description = 'Azure DevOps CLI'
+
+    def get_version_from_package(self):
+        self.version = importlib.metadata.version('azdevops_cli')
